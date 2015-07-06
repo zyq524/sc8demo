a8d597b8f171c0cc99091f8b8a6dd24103432e6f
Yuqing Zhang <zhangyuqing911@hotmail.com>
2015-07-06 14:02:03 +0200

    Added EXM dispatch service and contact service

---
+++ SC8Demo.EXM/DispatchService.cs
@@ -0,0 +1,28 @@
+﻿
+namespace SC8Demo.EXM
+{
+    using System;
+    using System.Collections.Generic;
+    using Sitecore.Modules.EmailCampaign.Recipients;
+
+    public class DispatchService
+    {
+        public void SendTriggeredMessageToXDB(Guid messageId, Guid contactId, bool usePreferredLanguage = false,
+        IDictionary<string, object> customPersonTokens = null)
+        {
+
+            RecipientId recipientId = RecipientRepository.GetDefaultInstance().ResolveRecipientId("xdb:" + contactId);
+
+            Sitecore.Modules.EmailCampaign.Application.Application.Instance.EmailDispatch.SendTriggered(messageId, recipientId, usePreferredLanguage, customPersonTokens);
+        }
+
+        public void SendTriggeredMessageToSitecoreUser(Guid messageId, Guid contactId, bool usePreferredLanguage = false,
+        IDictionary<string, object> customPersonTokens = null)
+        {
+
+            RecipientId recipientId = RecipientRepository.GetDefaultInstance().ResolveRecipientId("xdb:" + contactId);
+
+            Sitecore.Modules.EmailCampaign.Application.Application.Instance.EmailDispatch.SendTriggered(messageId, recipientId, usePreferredLanguage, customPersonTokens);
+        }
+    }
+}
---
+++ Sc8Demo.Core/ContactService.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Sc8Demo.Core
+{
+    using Sitecore.Analytics;
+    using Sitecore.Analytics.Model.Entities;
+
+    public class ContactService
+    {
+        public Sitecore.Analytics.Tracking.Contact CreateContact(string identifier, string firstName, string lastName, string email, string language)
+        {
+            Tracker.Current.Session.Identify(identifier);
+
+            var contactPreferences = Tracker.Current.Contact.GetFacet<IContactPreferences>("Preferences");
+            contactPreferences.Language = language;
+
+            var contactEmailAddresses = Tracker.Current.Contact.GetFacet<IContactEmailAddresses>("Emails");
+            contactEmailAddresses.Entries.Create("Preferred").SmtpAddress = email;
+            contactEmailAddresses.Preferred = "Preferred";
+
+            var personalInfo = Tracker.Current.Contact.GetFacet<IContactPersonalInfo>("Personal");
+            personalInfo.FirstName = firstName;
+            personalInfo.Surname = lastName;
+
+            return Tracker.Current.Contact;
+        }
+    }
+}
