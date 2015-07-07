e8dcc84a1b536fde7e2ad474b5215be2304f6f71
Yuqing Zhang <zhangyuqing911@hotmail.com>
2015-07-07 14:26:41 +0200

    Missing files.

---
+++ SC8Demo.Web/ClearSession.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SC8Demo.Web
+{
+    using SC8Demo.EXM;
+
+    public partial class ClearSession : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+        }
+    }
+}
